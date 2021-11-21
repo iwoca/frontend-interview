c17f3a6dc8d8489b5f396436c32e97082da640a4
The Basti <s.rother@iwoca.de>
2021-11-21 14:08:43 +0000

    feat: add task to create new applications and handle xcsrf tokens

---
+++ json-server/index.js
@@ -0,0 +1,29 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const db = require("./db.js");
+const router = jsonServer.router(db());
+const jsonServerMiddlewares = jsonServer.defaults();
+const csrfMiddleware = require("./csrfMiddleware.js");
+const { SERVER_XCSRF_TOKEN } = require("./config.js");
+
+const DELAY = 500;
+const API_BASE = "/api";
+const PORT = 3001;
+
+server.use(jsonServerMiddlewares);
+server.use(csrfMiddleware);
+
+server.use((req, res, next) => {
+  setTimeout(next, DELAY);
+});
+
+server.use(jsonServer.bodyParser);
+
+server.use(API_BASE, router);
+server.get("/auth/xcsrftoken", (req, res) => {
+  res.status(200).send(JSON.stringify({ xcsrftoken: SERVER_XCSRF_TOKEN }));
+});
+
+server.listen(PORT, () => {
+  console.log("JSON Server is running");
+});
